ab1cae9b86939ef5474c1302c15e531a081b5c6e
강 <ghang0501@gmail.com>
2025-02-09 00:37:34 +0900

    3.2 Navigation part Two

--- app/common/components/navigation.tsx
+++ app/common/components/navigation.tsx
@@ -1,12 +1,19 @@
+/*
+이 파일의 기본 셋팅에 대한 강의 => 노마드코더 Maker Master Class #3, 3.1~3.2
+*/
+
 import { Link } from "react-router";
 import { Separator } from "./ui/separator";
 import {
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuItem,
+  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
 } from "./ui/navigation-menu";
+import { cn } from "~/lib/utils";
 
 const menus = [
   {
@@ -134,14 +141,51 @@ export default function Navigation() {
           <NavigationMenuList>
             {menus.map((menu) => (
               <NavigationMenuItem key={menu.name}>
-                <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  {menu.items?.map((item) => (
-                    <NavigationMenuItem key={item.name}>
-                      <Link to={item.to}>{item.name}</Link>
-                    </NavigationMenuItem>
-                  ))}
-                </NavigationMenuContent>
+                {menu.items ? (
+                  <>
+                    <Link to={menu.to}>
+                      <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
+                    </Link>
+                    <NavigationMenuContent>
+                      <ul className="grid w-[600px] font-light gap-3 p-4 grid-cols-2">
+                        {menu.items?.map((item) => (
+                          <NavigationMenuItem
+                            key={item.name}
+                            className={cn([
+                              "select-none rounded-md transition-colors hover:bg-accent focus:bg-accent",
+                              item.to === "/products/promote" &&
+                                "col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20",
+                              item.to === "/jobs/submit" &&
+                                "col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20",
+                            ])}
+                          >
+                            {/* bg-accent는 실제로 강조하는 설정으로 shadcn에서 만들었음. cn()에 대한 설명은 utils.ts 파일 참고 */}
+                            <NavigationMenuLink asChild>
+                              {/*asChild의 의미는 우리가 shadcn에서 다운받은 NavigationBar의 모든 속성이 
+						  React요소인 Link에 스타일이 복제된다는 뜻(왜냐하면 우린 Link를 이용해 유저를 이동시켜야 하기 때문) 자세한 내용은 노마드코더 Maker #3, 3.2 1:43초 확인*/}
+                              <Link
+                                className="p-3 space-y-1 block leading-none no-underline outline-none"
+                                to={item.to}
+                              >
+                                <span className="text-sm font-medium leading-none">
+                                  {item.name}
+                                </span>
+                                <p className="text-sm leading-sung text-muted-foreground">
+                                  {item.description}
+                                </p>
+                              </Link>
+                            </NavigationMenuLink>
+                          </NavigationMenuItem>
+                        ))}
+                      </ul>
+                    </NavigationMenuContent>
+                  </>
+                ) : (
+                  <Link className={navigationMenuTriggerStyle()} to={menu.to}>
+                    {/* className은 해당 컴포넌트의 스타일을 적용하는 것이다. 자세한 내용은 #3, 3.2 11:04초 확인 */}
+                    {menu.name}
+                  </Link>
+                )}
               </NavigationMenuItem>
             ))}
           </NavigationMenuList>
--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+/*
+이 파일에 대한 기초적인 설명이 필요하다면? => 노마드코더 Maker Master Class #3, 3.2, 6:8초 확인
+
+________________________________________________________
+
+cn() 함수는 우리가 조금 더 편하게 tailwind css에서 조건을 만들 수 있도록 도와주는 함수이다. 
+예를 들어, 우리가 특정 페이지에서 특정 조건을 만족하는 경우 특정 스타일을 적용하고 싶다면, (nav에서 어떠한 url로 가는 내용을 강조하고 싶거나)
+cn() 함수를 사용하면 편리하다.
+*/
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
