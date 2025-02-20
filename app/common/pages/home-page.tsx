ee637a7944a591aa94b5e4c614ed3801968b820e
강 <ghang0501@gmail.com>
2025-02-20 14:54:47 +0900

    3.6 Post Card

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,5 +1,9 @@
-import { type MetaFunction } from "react-router";
+import { Link, type MetaFunction } from "react-router";
+import { Button } from "../components/ui/button";
 import { ProductCard } from "~/features/products/components/product-card";
+import { Card, CardFooter, CardHeader, CardTitle } from "../components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
+import { PostCard } from "~/features/community/components/post-card";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,15 +14,18 @@ export const meta: MetaFunction = () => {
 
 export default function HomePage() {
   return (
-    <div className="px-20">
+    <div className="px-20 space-y-40">
       <div className="grid grid-cols-3 gap-4">
         <div>
           <h2 className="text-5xl font-bold leading-tight tracking-tight">
             Today's Products
           </h2>
-          <p className="text-lg font-light text-foreground">
+          <p className="text-xl font-light text-foreground">
             The best products made by our community today
           </p>
+          <Button variant="link" asChild className="text-lg p-0 ">
+            <Link to="/products/leaderboard">Explore all products &rarr;</Link>
+          </Button>
         </div>
 
         {Array.from({ length: 10 }).map((_, index) => (
@@ -29,6 +36,29 @@ export default function HomePage() {
             commentsCount={12}
             viewsCount={12}
             upvotesCount={120}
+          />
+        ))}
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            Latest discussions
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            The latest discussions from our community
+          </p>
+          <Button variant="link" asChild className="text-lg p-0 ">
+            <Link to="/community">Explore all discussions &rarr;</Link>
+          </Button>
+        </div>
+        {Array.from({ length: 11 }).map((_, index) => (
+          <PostCard
+            id={`postId-${index}`}
+            title="What is the best productivity tool?"
+            author="Apple"
+            authorAvatarUrl="https://github.com/apple.png"
+            category="productivity"
+            createdAt="12 hours ago"
           />
         ))}
       </div>
---
+++ app/features/community/components/post-card.tsx
@@ -0,0 +1,58 @@
+import { Link } from "react-router";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "~/common/components/ui/avatar";
+import { Button } from "~/common/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+} from "~/common/components/ui/card";
+
+interface PostCardProps {
+  id: string;
+  title: string;
+  author: string;
+  authorAvatarUrl: string;
+  category: string;
+  createdAt: string;
+}
+
+export function PostCard({
+  id,
+  title,
+  author,
+  category,
+  createdAt,
+  authorAvatarUrl,
+}: PostCardProps) {
+  return (
+    <Link to={`/community/${id}`}>
+      <Card className="bg-transparent hover:bg-card/50 transition-colors">
+        <CardHeader className="flex flex-row gap-2 items-center">
+          <Avatar className="size-14">
+            <AvatarFallback>{author[0]}</AvatarFallback>
+            <AvatarImage src={authorAvatarUrl} />
+          </Avatar>
+          <div className="space-y-2">
+            <CardTitle>{title}</CardTitle>
+            <div className="flex gap-2 text-sm leading-tight text-muted-foreground">
+              <span>{author} on</span>
+              <span>{category}</span>
+              <span>•</span>
+              <span>{createdAt}</span>
+            </div>
+          </div>
+        </CardHeader>
+        <CardFooter className="flex justify-end">
+          <Button variant="link" asChild>
+            <Link to={`/community/${id}`}>Reply &rarr;</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </Link>
+  );
+}
